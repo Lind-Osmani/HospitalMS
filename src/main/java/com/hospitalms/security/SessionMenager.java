00cc32eabd31d0bf04af8bb9ef7b7316fd81e72b
Lorik Hamiti <lorik.hamiti@student.uni-pr.edu>
2026-05-24 22:58:40 +0200

    Implement SessionManager for user authentication
    SessionManager perdoret per perdorim te balancuar per te gjithe user-at

--- src/main/java/com/hospitalms/security/SessionMenager.java
+++ src/main/java/com/hospitalms/security/SessionMenager.java
@@ -1,1 +1,46 @@
+package com.hospitalms.security;
 
+import com.hospitalms.enums.UserRole;
+
+public class SessionManager {
+
+    private static String currentUsername;
+
+    private static UserRole currentUserRole;
+
+    private static boolean loggedIn = false;
+
+
+    public static void login(String username, UserRole role) {
+
+        currentUsername = username;
+        currentUserRole = role;
+        loggedIn = true;
+
+    }
+
+
+    public static void logout() {
+
+        currentUsername = null;
+        currentUserRole = null;
+        loggedIn = false;
+
+    }
+
+
+    public static String getCurrentUsername() {
+        return currentUsername;
+    }
+
+
+    public static UserRole getCurrentUserRole() {
+        return currentUserRole;
+    }
+
+
+    public static boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+}
